b3174e4da5b30e44acf1d1b7bba1ce8ac5a21524
Leehom <lh1649896772@163.com>
2017-03-29 15:09:03 +0800

    对所有主机今次那个初始化

--- os/init_all_hosts.sh
+++ os/init_all_hosts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-httppost=`cat /etc/httpd/conf/httpd.conf |grep "Listen " |grep -v "#" |awk '{print $2}'`
-httpid=`hostname -i`
+http_post=`cat /etc/httpd/conf/httpd.conf |grep "Listen " |grep -v "#" |awk '{print $2}'`
+http_id=`hostname -i`
 
-cat ./ip.txt|while read line;
+cat /etc/ip.txt|while read line;
 do
 hn=`echo $line|awk '{print $1}'`
 pw=`echo $line|awk '{print $2}'`
@@ -17,12 +17,12 @@ spawn ssh $hn
 	"*assword:" { send "$pw\n" } 
 	}
 		expect "*#*"
-	send "wget http://$httpid\:$httppost/sugo_yum/SG/centos6/1.0/init_centos6.sh\n"
+	send "wget http://$http_id\:$http_post/sugo_yum/SG/centos6/1.0/init_centos6.sh\n"
         expect "*#*"
 	send "chmod 755 init_centos6.sh\n"
         expect "*#*"
-	send "./init_centos6.sh -hostname $hn -yum_baseurl http://$httpid\:$httppost/sugo_yum\n"
-	send "rm -rf init_centos6.sh\n"
+	send "./init_centos6.sh -hostname $hn -yum_baseurl http://$http_id\:$http_post/sugo_yum\n"
+	send "rm -rf init_centos6.sh\r"
 		expect "*]#*"
 EOF
 done
