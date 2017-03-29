0a468308e5b973bff7aa68043ed875091570c6de
Leehom <lh1649896772@163.com>
2017-03-29 15:09:22 +0800

    创建数据存放目录

--- os/create_datadir.sh
+++ os/create_datadir.sh
@@ -5,12 +5,13 @@ if [ "$1" = "" ]
 then
 	mkdir /data1 /data2
 
-	cat ./ip.txt |while read line;
+	cat /etc/ip.txt |while read line;
 	do
 	hn=`echo $line|awk '{print $1}'`
 	pw=`echo $line|awk '{print $2}'`
 	
 	/usr/bin/expect <<-EOF
+	set timeout 100000
 	spawn ssh $hn
 		expect {
 		"*yes/no*" { send "yes\n"
@@ -20,32 +21,45 @@ then
 			expect "*#*"
 		send "mkdir /data1 /data2\n"
 			expect "*]#*"
+		send "yum install -y wget\r"
+		expect "*]#*"
+		send "yum install -y ntp\r"
+		expect "*]#*"
+		send "yum install -y openssh-clients\r"
+			expect "*]#*"
 	EOF
 	done
 
 else
-	mkdir $1/data1 $1/data2
+	mkdir -p $1/data1 $1/data2
 	ln -s $1/data1 /data1
 	ln -s $1/data2 /data2
 	
-	cat ./ip.txt |while read line;
+	cat /etc/ip.txt |while read line;
 	do
 	hn=`echo $line|awk '{print $1}'`
 	pw=`echo $line|awk '{print $2}'`
 	
 	/usr/bin/expect <<-EOF
+	set timeout 100000
 	spawn ssh $hn
 		expect {
 		"*yes/no*" { send "yes\n"
 		expect "*assword:" { send "$pw\n" } }
 		"*assword:" { send "$pw\n" } 
 		}
-			expect "*#*"
-		send "mkdir $1/data1 $1/data2\n"
-			expect "*#*"
+			expect "*]#*"
+		send "mkdir -p $1/data1 $1/data2\n"
+			expect "*]#*"
 		send "ln -s $1/data1 /data1\n"
-			expect "*#*"
+			expect "*]#*"
 		send "ln -s $1/data2 /data2\n"
+			expect "*]#*"
+		send "yum install -y wget\r"
+		expect "*]#*"
+		send "yum install -y ntp\r"
+		expect "*]#*"
+		send "yum install -y openssh-clients\r"
 			expect "*]#*"
 	EOF
 	done
