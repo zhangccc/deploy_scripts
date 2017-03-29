843471db5f9fb500e84656eb1a6c41b3cd0ef1f2
Leehom <lh1649896772@163.com>
2017-03-29 15:08:16 +0800

    控制主机初始化过程

--- os/init_process.sh
+++ os/init_process.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-httpid=`hostname -i`
+http_id=`hostname -i`
 hn=`hostname`
-httppost=`cat /etc/httpd/conf/httpd.conf |grep "Listen " |grep -v "#" |awk '{print $2}'`
+http_post=`cat /etc/httpd/conf/httpd.conf |grep "Listen " |grep -v "#" |awk '{print $2}'`
 
 #ambari-server主机参数优化（包括THP、防火墙、设置ssh免密码登录、安装jdk等）
-./init_centos6.sh -hostname $hn -yum_baseurl http://$httpid:$httppost/sugo_yum
+cd /root/deploy_scripts/os
+./init_centos6.sh -hostname $hn -yum_baseurl http://$http_id:$http_post/sugo_yum
 
 #将/root/.ssh/id_rsa.pub放到yum源目录下的SG/Centos6/1.0/:
 rm -rf /var/www/html/sugo_yum/SG/centos6/1.0/id_rsa.pub
