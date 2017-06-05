af4082aaf424863bd339190bc258fa5e6672f2ce
leehom <lh1649896772@163.com>
2017-06-05 17:28:47 +0800

    安装、注册ambari-agent

---
+++ centos6/ambari-agent/add_agent.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+#coding=utf-8
+
+import json, os, sys, time
+import urllib2
+
+# need to be altered
+cluster_name = sys.argv[1]
+ambari_server_ip = sys.argv[2]
+
+agent_file = "hosts"
+a = open(agent_file, "r+")
+hosts = []
+
+for line in a:
+    host = line.split(" ")[0]
+    hosts.append(host)
+
+
+sshkey_file = "/root/.ssh/id_rsa"
+f = open(sshkey_file, "r+")
+sshkey = f.read().strip('\n')
+url0 = "http://" + ambari_server_ip + ":8080/api/v1/bootstrap"
+
+# install ambari-agent
+data1 = {}
+serviceinfo = {}
+data1["verbose"] = "true"
+data1["hosts"] = hosts
+data1["sshKey"] = sshkey
+data1["user"] = "root"
+data1["userRunAs"] = "root"
+
+request = urllib2.Request(url0)
+request.add_header('Authorization', 'Basic YWRtaW46YWRtaW4=')
+request.add_header('Content-Type', 'application/json')
+request.add_header('X-Requested-By', 'ambari')
+response = urllib2.urlopen(request, json.dumps(data1), 10)
+print response.read()
+f.close()
+
+time.sleep(5)
+
+# register ambari-agent
+for host in hosts:
+    url1 = "http://" + ambari_server_ip + ":8080/api/v1/clusters/" + cluster_name + "/hosts/" + host
+    request_register = urllib2.Request(url1)
+    request_register.add_header('Authorization', 'Basic YWRtaW46YWRtaW4=')
+    request_register.add_header('Content-Type', 'application/json')
+    request_register.add_header('X-Requested-By', 'ambari')
+    response2 = urllib2.urlopen(request_register, "", 60)
+    print response2.read()
---
+++ centos6/ambari-agent/ambari-agent脚本使用说明.md
@@ -0,0 +1,26 @@
+# ambari-agent脚本使用说明 #
+
+## 1. 准备 ##
+### hosts文件
+hosts文件用于写入需要添加的agent的主机名, root用户的密码，该主机的IP地址
+按行写入，每行代表一个主机(ambari-agent)，各项目之间以空格“ ”分割
+例：
+```
+test3.sugo.vm 123456789 192.168.0.110
+```
+
+## 2. 脚本使用 ##
+### pre_add_agent.sh
+此脚本用于安装agent之前的主机准备，包括安装jdk、配置ambari-server到该agent的ssh免密码登录、系统优化等，且jdk、ssh免密码可选择性安装
+具体使用可通过如下命令进行查看
+```
+./pre_add_agent.sh -help
+```
+
+### add_agent.py
+此脚本用于安装、注册ambari-agent，会在hosts文件中列出的主机上安装注册agent，运行此脚本需输入参数：集群名称 ambari-server的IP地址
+例：
+```
+python add_agent.py testCluster 192.168.0.110
+```
+注：此脚本包含安装和注册两个部分，由于主机的配置等因素不同，安装所需时间也会不同，而注册需要安装完成后才可执行，此处设置安装等待时间为5秒，若因配置原因，脚本注册部分执行失败，重复执行该脚本即可
